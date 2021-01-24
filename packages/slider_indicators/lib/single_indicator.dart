85d1cb54a5cc691573dbcaa6865ec838faf1c1da
Tigran <tigran.muradyan140201@gmail.com>
2021-01-24 12:54:20 +0400

    Added SliderIndicators package

---
+++ packages/slider_indicators/lib/single_indicator.dart
@@ -0,0 +1,31 @@
+part of slider_indicators;
+
+class SingleIndicator extends StatelessWidget {
+  const SingleIndicator(
+    this.isActive, {
+    Key key,
+    this.paddingBetween = 3,
+    this.size = 10,
+  }) : super(key: key);
+  final bool isActive;
+  final double paddingBetween;
+  final double size;
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData _themeContext = Theme.of(context);
+    final Color _bgColor = isActive ? _themeContext.primaryColor : _themeContext.disabledColor.withOpacity(0.3);
+
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: paddingBetween),
+      child: Container(
+        width: isActive ? size + 10 : size,
+        height: size,
+        decoration: BoxDecoration(
+          color: _bgColor,
+          borderRadius: BorderRadius.circular(20),
+        ),
+      ),
+    );
+  }
+}
---
+++ packages/slider_indicators/lib/slider_indicators.dart
@@ -0,0 +1,20 @@
+library slider_indicators;
+
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+part 'single_indicator.dart';
+
+class SliderIndicators extends StatelessWidget {
+  const SliderIndicators(this.page, this.totalPages, {Key key, this.singleIndicator}) : super(key: key);
+  final int page;
+  final int totalPages;
+  final Widget singleIndicator;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [for (int i = 0; i < totalPages; i++) singleIndicator ?? SingleIndicator(page == i)],
+    );
+  }
+}
