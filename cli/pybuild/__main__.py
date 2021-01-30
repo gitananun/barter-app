237d46143c0a6998f0a475be43791dd3052359ab
Tigran <tigran.muradyan140201@gmail.com>
2021-01-30 18:53:11 +0400

    App Build

--- cli/pybuild/__main__.py
+++ cli/pybuild/__main__.py
@@ -1,14 +1,16 @@
 
 import os
 
+
 def main():
     # Change the app mode
-   
+
     mode = input('Please enter build app mode: ')
     app_modes = ('dev', 'Dev', 'release', 'Release')
 
     if(mode not in app_modes):
-        print('\033[91m' + 'App build modes: ' + '\033[93m' + str(app_modes) + '\033[93m')
+        print('\033[91m' + 'App build modes: ' +
+              '\033[93m' + str(app_modes) + '\033[93m')
         return
 
     code = "import 'package:barter/ui/enums/app_mode.dart'; const AppMode APP_MODE = AppMode.{mode};".format(
@@ -17,8 +19,10 @@ def main():
 
     file = open('lib/env.dart', 'w')
     file.write(code)
+    file.close()
 
-    print('\033[94m' + 'Changed successfully to ' + '\033[92m' + mode + '\033[0m')
+    print('\033[94m' + 'Changed successfully to ' +
+          '\033[92m' + mode + '\033[0m')
     print('\n')
 
     # Run CLI commands
@@ -26,17 +30,19 @@ def main():
     ways = ('git', 'Git', 'local', 'Local')
 
     if(way not in ways):
-        print('\033[91m' + 'Valid ways: ' + '\033[93m' + str(ways) + '\033[93m')
+        print('\033[91m' + 'Valid ways: ' +
+              '\033[93m' + str(ways) + '\033[93m')
         return
 
     print('\033[92m' + 'Build started...' + '\033[0m')
 
     if(way == 'git' or way == 'Git'):
         os.system('git add . && git commit -m "App Build"')
-        os.system('git push')    
+        os.system('git push')
         return
 
-    os.system('flutter clean && flutter build apk')    
-    
+    os.system('flutter clean && flutter build apk')
+
+
 if __name__ == '__main__':
     main()
---
+++ lib/env.dart
@@ -0,0 +1,1 @@
+import 'package:barter/ui/enums/app_mode.dart'; const AppMode APP_MODE = AppMode.Release;
